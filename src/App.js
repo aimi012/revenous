edc84f144e068f3f5697ff046bb38edfa3c4e8ae
aimi012 <aimiamirul01@gmail.com>
2023-03-25 15:55:05 +0800

    nnn

--- src/App.js
+++ src/App.js
@@ -2,14 +2,28 @@ import React from 'react';
 // import ReactDOM from 'react';
 import './App.css';
 import BusinessList from './components/BusinessList/BusinessList';
-import SearchBar from './components/SeacrhBar/SeacrhBar';
+import SearchBar from './components/SearchBar/SearchBar';
+
+const busisness = {
+  imageSrc: 'https://content.codecademy.com/programs/react/ravenous/pizza.jpg',
+  name: 'MarginOtto Pizzeria',
+  address: '1010 Paddington Way', 
+  city: 'Flavortown',
+  state: 'NY',
+  zipCode: '10101',
+  category: 'Italian',
+  rating: 4.5,
+  reviewCount: 90
+}
+
+const busisnesses = [busisness,busisness,busisness,busisness,busisness,busisness]
 
 function App() {
   return (
     <div class="App">
   <h1>ravenous</h1>
   <SearchBar/>
-  <BusinessList/>
+  <BusinessList busisness={busisnesses}/>
 </div>
   );
 }
--- src/components/BusinessList/BusinessList.jsx
+++ src/components/BusinessList/BusinessList.jsx
@@ -1,22 +1,22 @@
-import React from 'react'
-import './BusinessList.css'
-import Business from '../Busisness/Busisness'
+import React from "react";
+import "./BusinessList.css";
+import Busisness from "../Business/Busisness";
 
 class BusinessList extends React.Component {
-    render() {
-        return(
-            <>
-            <div className="BusinessList">
-                <Business/>
-                <Business/>
-                <Business/>
-                <Business/>
-                <Business/>
-                <Business/>
-</div>
-            </>
-        )
-    }
+  render() {
+    return (
+      <>
+        <div className="BusinessList">
+            {
+                this.props.busisnesses.map((businiess)=> {
+                    return <Busisness busisness={businiess}/>
+
+                })
+            }
+        </div>
+      </>
+    );
+  }
 }
 
 export default BusinessList;
